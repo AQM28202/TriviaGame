a9fe2101cdb668862338a3a69c4f1677a6c47657
Anthony McPherson <aqm28202@gmail.com>
2019-08-17 17:31:53 -0400

    <wiring up start button to alert as a checkpoint>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,10 @@
 // Player is only allowed to choose one answer per question
 // Player clicks submit button
 // HTML is updated to display player's number of correct and incorrect answers 
+
+$(document).ready(function(){
+
+    $("#start-button").on("click", function(){
+        alert("Working");
+      });
+    });
