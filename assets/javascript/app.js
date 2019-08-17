66eac3c7906beb0f02a9ce6cde49586ae5ca12d2
Anthony McPherson <aqm28202@gmail.com>
2019-08-16 20:03:03 -0400

    <pseudo coding>

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// Click button to start (Displays questions and starts 60 second timer)
+// Player answers 10 multiple choice questions
+// Player is only allowed to choose one answer per question
+// Player clicks submit button
+// HTML is updated to display player's number of correct and incorrect answers 
