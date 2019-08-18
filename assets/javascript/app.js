a991a433e6a33bd818a54ad3d28add36be86bfe4
Anthony McPherson <aqm28202@gmail.com>
2019-08-17 20:58:11 -0400

    <reformatting html text>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@
 
     $("#start-button").on("click", function(){
         $("#splash-page").hide();
-        $("#questions-display").html("<h3>" + questionList[0].question + "</h3>");
+        $("#questions-display").html("<p>" + questionList[0].question + "</p>");
       });
     
 
@@ -15,37 +15,37 @@
 var questionList =
 [
   {
-    question: "Which King of Israel murdered his own soldier to cover up that he had impregnated that soldier's wife?",
+    question: "1. Which King of Israel murdered his own soldier to cover up that he had impregnated that soldier's wife?",
     answers: ["King Uzziah", "King David", "King Josiah"],
     correct: "King David"
   },
     {
-    question: "Who ordered a baby to be cut in half in order to resolve a custody dispute?",
+    question: "2. Who ordered a baby to be cut in half in order to resolve a custody dispute?",
     answers: ["King Solomon", "King Herod", "Pharaoh"],
     correct: "King Solomon"
   },
   {
-    question: "Who was reportedly stripped naked, beaten, killed and mocked as 'King of the Jews?'",
+    question: "3. Who was reportedly stripped naked, beaten, killed and mocked as 'King of the Jews?'",
     answers: ["Joshua", "John", "Jesus"],
     correct: "Jesus"
   },
   {
-    question: "Who ordered the execution of all male children two years old and under in the vicinity of Bethlehem?",
+    question: "4. Who ordered the execution of all male children two years old and under in the vicinity of Bethlehem?",
     answers: ["King Manasseh", "King Herod", "King Jehoiakim"],
     correct: "King Herod"
   },
   {
-    question: "Who was rumored to have 700 wives and 300 concubines?",
+    question: "5. Who was rumored to have 700 wives and 300 concubines?",
     answers: ["King Hezekiah", "King Ahab", "King Solomon"],
     correct: "King Solomon"
   },
   {
-    question: "Which King did nothing after finding out that his son had raped his daughter?",
+    question: "6. Which King did nothing after finding out that his son had raped his daughter?",
     answers: ["King David", "King Jehoshaphat", "King Jeroboam"],
     correct: "King David"
   },
   {
-    question: "As punishment for praying, who threw a prophet into a lion's den?",
+    question: "7. As punishment for praying, who threw a prophet into a lion's den?",
     answers: ["King Jehu", "King Darius", "King Rehoboam"],
     correct: "King Darius"
   },
